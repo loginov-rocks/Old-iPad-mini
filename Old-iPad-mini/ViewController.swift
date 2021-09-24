6f404eeae7501eae5927952507a88bf624214b0c
Danila Loginov <danila@loginov.rocks>
2021-09-24 10:49:01 +0300

    Initial Commit

---
+++ Old-iPad-mini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Old-iPad-mini
+//
+//  Created by Danila on 9/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
